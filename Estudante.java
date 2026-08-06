98b7f2c9e7ce86bbf82a75d4af9ce177e35d2f89
vissinos <viniciusdelimagrub@gmail.com>
2026-08-06 09:38:49 -0300

    Add files via upload

---
+++ Estudante.java
@@ -0,0 +1,13 @@
+class Estudante {
+
+    //variaveis
+    private String nome;
+    private double[] notas;
+
+    public Estudante(String nome){
+        this.nome = nome;
+        notas = new double[5];
+    }
+
+    public void 
+}
