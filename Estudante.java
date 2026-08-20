212ef82b2ae67dee44f36690143c4dfdf4c3830b
vissinos <viniciusdelimagrub@gmail.com>
2026-08-20 20:44:19 -0300

    adicionei a possibilidade de se fazer a média ponderada

---
+++ Estudante.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Estudante {
+    private String nome;
+    private double[] notas;
+
+    public Estudante(String nome) {
+        this.nome = nome;
+        this.notas = new double[5];
+    }
+
+    public void insereNotas() {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < notas.length; i++) {
+            System.out.println("Digite a nota " + (i + 1) + " de " + nome + ":");
+            notas[i] = sc.nextDouble();
+        }
+    }
+
+    public double calculaMedia() {
+        double soma = 0;
+        for (int i = 0; i < notas.length; i++) {
+            soma = soma + notas[i];
+        }
+        return soma / notas.length;
+    }
+
+    public double calculaMedia(int[] pesos) {
+        double somaNotas = 0;
+        int somaPesos = 0;
+
+        for (int i = 0; i < notas.length; i++) {
+            somaNotas = somaNotas + (notas[i] * pesos[i]);
+            somaPesos = somaPesos + pesos[i];
+        }
+
+        return somaNotas / somaPesos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double[] getNotas() {
+        return notas;
+    }
+
+    public double menorNota() {
+        double menor = notas[0];
+        for (int i = 1; i < notas.length; i++) {
+            if (notas[i] < menor) {
+                menor = notas[i];
+            }
+        }
+        return menor;
+    }
+}
