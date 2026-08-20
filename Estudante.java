4c5157308fbf30a68398f39a16f64b6a25fd53f9
vissinos <viniciusdelimagrub@gmail.com>
2026-08-19 23:24:35 -0300

    adicionei inserir notas

--- Estudante.java
+++ Estudante.java
@@ -1,13 +1,19 @@
-class Estudante {
+import java.util.Scanner;
 
-    //variaveis
+public class Estudante {
     private String nome;
     private double[] notas;
 
-    public Estudante(String nome){
+    public Estudante(String nome) {
         this.nome = nome;
-        notas = new double[5];
+        this.notas = new double[5];
     }
 
-    public void 
+    public void insereNotas() {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < notas.length; i++) {
+            System.out.println("Digite a nota " + (i + 1) + " de " + nome + ":");
+            notas[i] = sc.nextDouble();
+        }
+    }
 }
