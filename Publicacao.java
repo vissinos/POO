1b48be460e4d48a1a7b1ea5f878bcd596417e303
vissinos <viniciusdelimagrub@gmail.com>
2026-08-22 23:17:43 -0300

    criei a superclasse
    dados comuns entre livro e periodico

---
+++ Publicacao.java
@@ -0,0 +1,11 @@
+public class Publicacao {
+    private String titulo;
+
+    public Publicacao(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+}
